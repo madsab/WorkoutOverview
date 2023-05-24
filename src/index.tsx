d5bf7579f9c38ffc32d479cb67da8fae6d7c0506
madsab <mads.barnes@hotmail.com>
2023-05-24 21:50:09 +0200

    Setting up typescript

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './pages/App';
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}/>
+    </Routes>
+  </BrowserRouter>
+);
+
+
