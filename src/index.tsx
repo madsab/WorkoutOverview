04730528f8a7eae1d4ebe005255dd2b9d183cebc
madsab <mads.barnes@hotmail.com>
2023-05-25 09:49:37 +0200

    Everything setup en good to go

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
+
+import './index.css';
+import { BrowserRouter} from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './pages/App';
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />}/>
-    </Routes>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>
 );
 
