117e5c63c769214956a324224b3c3197e9a61925
madsab <mads.barnes@hotmail.com>
2023-05-28 13:37:47 +0200

    new Homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
-
 const HomePage = () => {
-    return (
-        <div>
-            Homepage
-        </div>
-    )
-}
+  return <div>This is Homepage</div>;
+};
 
 export default HomePage;
