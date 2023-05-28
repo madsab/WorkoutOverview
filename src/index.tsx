b44f2312ee7d9aa3beca67fbb0a959fabaf56a7c
madsab <mads.barnes@hotmail.com>
2023-05-28 13:40:40 +0200

    setup complete

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import './index.css';
-import { BrowserRouter} from 'react-router-dom';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './pages/App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Failed to find the root element');
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <BrowserRouter>
@@ -15,5 +14,3 @@ root.render(
     </React.StrictMode>
   </BrowserRouter>
 );
-
-
