0e50b1d162658097754648593511f329679b7f12
madsab <mads.barnes@hotmail.com>
2023-06-12 20:54:19 +0200

    added a barchart

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { FC } from "react";
+import BarChart from "../components/BarChart";
 
 const HomePage = () => {
-  return <div>This is Homepage</div>;
+  const data = [
+    { day: "Monday", value: 2 },
+    { day: "Monday", value: 2 },
+    { day: "Monday", value: 4 },
+  ];
+  return (
+    <>
+      <div className=" border-solid border-2 border-red-600 mt-5 mb-20 h-full relative">
+        <BarChart data={data} height={"24"} />
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
