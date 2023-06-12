d6478268870079b54862a4287691aaf483159adb
madsab <mads.barnes@hotmail.com>
2023-06-12 20:55:24 +0200

    new component

---
+++ src/atoms/ContentContainer.tsx
@@ -0,0 +1,11 @@
+
+
+const ContentContainer = () => {
+    return (
+        <>
+        <div className="bg-[#1E4D5C] w-full text-white drop-shadow-md "></div>
+        </>
+    )
+}
+
+export default ContentContainer;
